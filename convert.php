10237a79062353784607fa849e7206a12f5bc6bf
noomkrub <noomkrub@users.noreply.github.com>
2021-04-10 17:11:20 +0700

    Update convert.php
    Add option for reduce number of waypoint and guard width

--- convert.php
+++ convert.php
@@ -1,10 +1,12 @@
 <?php
+header("content-type:text/plain");
 if (php_sapi_name() != "cli") {
-	echo "This program is to be called via commandline\n\n<br><br>";
-	echo "Usage: php /path/to/script/convert.php input.gpx output.rtz";
+	echo "This program is to be called via commandline\n\n";
+	echo "Usage: php /path/to/script/convert.php input.gpx output.rtz step width\n";
+	echo "option: step width\n\tstep: distant between each waypoint\n\twidth: route width";
 	exit;
 }
-//var_dump($argv);
+
 if(!isset($argv[1])){
 echo "Error: Need input file name\nUsage: php /path/to/script/convert.php input.gpx output.rtz\n\n";
 	exit;
@@ -13,14 +15,15 @@ if(!isset($argv[2])){
 echo "Error: Need output file name\nUsage: php /path/to/script/convert.php input.gpx output.rtz\n\n";
 	exit;
 }
+if (isset($argv[3])){$space=$argv[3];}else{$space=0;}
+if (isset($argv[4])){$width=$argv[4];}else{$width=0.37*2;}
 
-header("content-type=text/plain");
 $xml=simplexml_load_file($argv[1]);
 
 $op="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!--route node-->\n<route version=\"1.0\" xmlns=\"http://www.cirm.org/RTZ/1/0\">\n  <!--route node-->\n";
 $op.="  <routeInfo routeName=\"";
-  $op.=$xml->trk->name;
-  $op.='" optimizationMethod="MAX speed">
+$op.=$xml->trk->name;
+$op.='" optimizationMethod="MAX speed">
     <extensions>
       <extension manufacturer="Furuno" name="AdditionalRouteInfo" version="1.0">
         <property income="0" channelLimitMode="0" safetyContour="8" ukcLimit="30.000000"/>
@@ -31,8 +34,8 @@ $op.="  <routeInfo routeName=\"";
   <waypoints>';
 $wpt_num=1;
 foreach ($xml->trk->trkseg->trkpt as $wpt){
-if ($wpt_num==1){
-$op.='
+	if ($wpt_num==1){
+		$op.='
     <!--No.'.$wpt_num.' waypoint-->
     <waypoint id="'.$wpt_num.'" name="" radius="0.010000">
       <position lat="';
@@ -41,9 +44,16 @@ $op.='
       $op.=$wpt['lon'];
       $op.='"/>
     </waypoint>';
-}
-else {
-$op.='
+    	$lastlat=$wpt['lat'];
+    	$lastlon=$wpt['lon'];
+    	$wpt_num++;
+		}
+	else {
+		$dlat = abs($wpt['lat']-$lastlat);
+		$dlon = abs($wpt['lon']-$lastlon);
+		$dist = sqrt(($dlat*$dlat) + ($dlon*$dlon));
+		if ($dist >= $space){
+			$op.='
     <!--No.'.$wpt_num.' waypoint-->
     <waypoint id="'.$wpt_num.'" name="" radius="0.010000">
       <position lat="';
@@ -51,16 +61,23 @@ $op.='
       $op.='" lon="';
       $op.=$wpt['lon'];
       $op.='"/>
-      <leg portsideXTD="0.377970" starboardXTD="0.377970" safetyContour="8" geometryType="Loxodrome" speedMax="11.000000" draughtForward="10.000000" draughtAft="10.000000" staticUKC="30.000000"/>
+      <leg portsideXTD="';
+      $op.=$width/2;
+      $op.='" starboardXTD="';
+      $op.=$width/2;
+      $op.='" safetyContour="8" geometryType="Loxodrome" speedMax="11.000000" draughtForward="10.000000" draughtAft="10.000000" staticUKC="30.000000"/>
       <extensions>
         <extension manufacturer="Furuno" name="AdditionalLegInfo" version="1.0">
           <property margin="0.000000" parallelLine1="0.000000" parallelLine2="0.000000"/>
         </extension>
       </extensions>
     </waypoint>';
+    		$lastlat=$wpt['lat'];
+    		$lastlon=$wpt['lon'];
+    		$wpt_num++;
+			}
+		} // end else
 
-}
-    $wpt_num++;
 	} //end foreach
 
 $op.='
